f6154534c9db301cb8793f8b74cbb2bcca1b5422
Nathan  Hare <me@laspruca.nz>
2023-07-08 17:21:20 +1200

    Remove appp.postcss from play/+layout.svelte

--- src/routes/play/+layout.svelte
+++ src/routes/play/+layout.svelte
@@ -3,7 +3,6 @@
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
 
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
