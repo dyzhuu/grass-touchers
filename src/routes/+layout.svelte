8cd980bf18f3d2551a9dd2ae94cbf3366688f336
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-07-08 17:02:02 +1200

    scuffed buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,6 @@
 
 <script lang='ts'>
 	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
@@ -66,19 +65,43 @@
 
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
-	function switchPage(page:string) {
-        let pageButton = document.getElementById(`${page}Button`) as HTMLElement;
-        pageButton.classList.add("selected");
+	import {page} from "$app/stores";
 
-        if (page == "map") {
-            pageButton = document.getElementById("leaderboardButton") as HTMLElement;
-        } else {
-            pageButton = document.getElementById("mapButton") as HTMLElement;
-        }
-        pageButton.classList.remove("selected");
-    }
+	$: map = $page.url.pathname == "/play";
+	$: leaderboard = $page.url.pathname == "/play/leaderboard";
 
-	function uploadPicture() {
-		alert("100 points added!");
-	}
+	$: console.log(map);
+	
 </script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">(Logo)</svelte:fragment>
+			<svelte:fragment slot="trail">(Camera)</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer"> 
+		<div class="h-20 flex ">
+			<a href="/play">
+				<div class="flex justify-center h-full w-[50dvw] p-1">
+					<button type="button" class="btn w-full rounded-md text-black {map ? 'bg-primary-800' : 'bg-primary-500'}" >Map</button>
+				</div>
+			</a>
+
+			<a data-sveltekit-reload href="/play/leaderboard">
+				<div class="flex justify-center w-[50dvw] h-full p-1">
+					<button type="button" class="btn w-full rounded-md {leaderboard ? 'bg-primary-500': 'bg-primary-800'}">L</button>
+				</div>
+			</a>
+		</div>
+	</svelte:fragment>
+</AppShell>
