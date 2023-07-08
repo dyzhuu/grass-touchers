ceb4d55d68356c90c93014533fcee55d99e5db6f
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-07-08 18:42:19 +1200

    for now

--- src/routes/play/leaderboard/+page.svelte
+++ src/routes/play/leaderboard/+page.svelte
@@ -3,62 +3,45 @@
     import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
 
     const you = [
-        { position: 100, name: 'hee', score: '1' },
+        { ranking: 100, name: 'hee', score: '1' },
     ]
 
     const sourceData = [
-        { position: 1, name: 'Hydrogen', score: '100' },
-        { position: 2, name: 'Helium', score: '90' },
-        { position: 3, name: 'Lithium', score: '80' },
-        { position: 4, name: 'Beryllium', score: '70' },
-        { position: 5, name: 'Boron', score: '60' },
-        { position: 6, name: 'Hydrogen', score: '100' },
-        { position: 7, name: 'Helium', score: '90' },
-        { position: 8, name: 'Lithium', score: '80' },
-        { position: 9, name: 'Beryllium', score: '70' },
-        { position: 10, name: 'Boron', score: '60' },
-        { position: 11, name: 'Hydrogen', score: '100' },
-        { position: 12, name: 'Helium', score: '90' },
-        { position: 13, name: 'Lithium', score: '80' },
-        { position: 14, name: 'Beryllium', score: '70' },
-        { position: 15, name: 'Boron', score: '60' },
-        { position: 15, name: 'Boron', score: '60' },
-        { position: 15, name: 'Boron', score: '60' },
-        { position: 15, name: 'Boron', score: '60' },
-        { position: 16, name: 'Boron', score: '60' },
-        { position: 17, name: 'Boron', score: '60' },
+        { ranking: 1, name: 'Hydrogen', score: '100' },
+        { ranking: 2, name: 'Helium', score: '90' },
+        { ranking: 3, name: 'Lithium', score: '80' },
+        { ranking: 4, name: 'Beryllium', score: '70' },
+        { ranking: 5, name: 'Boron', score: '60' },
+        { ranking: 6, name: 'Hydrogen', score: '100' },
+        { ranking: 7, name: 'Helium', score: '90' },
+        { ranking: 8, name: 'Lithium', score: '80' },
+        { ranking: 9, name: 'Beryllium', score: '70' },
+        { ranking: 10, name: 'Boron', score: '60' },
+        { ranking: 11, name: 'Hydrogen', score: '100' },
+        { ranking: 12, name: 'Helium', score: '90' },
+        { ranking: 13, name: 'Lithium', score: '80' },
+        { ranking: 14, name: 'Beryllium', score: '70' },
     ];
 
-    const leaderboard: TableSource = {
-        // A list of heading labels.
-        head: ['Rank', 'Name', 'Score'],
-        // The data visibly shown in your table body UI.
-        body: tableMapperValues(sourceData, ['position', 'name', 'score']),
-    }
-
-    const youTable: TableSource = {
-        // A list of heading labels.
-        head: [],
-        // The data visibly shown in your table body UI.
-        body: tableMapperValues(you, ['position', 'name', 'score']),
-    }
 </script>
-
-<div id="global">
-    <Table source={leaderboard}/>
+<div class="flex justify-center">
+  <table class="table-auto w-full border border-gray-300 rounded-lg max-w-[600px]">
+    <thead class="bg-primary-500 text-white">
+      <tr>
+        <th class="px-4 py-2">Ranking</th>
+        <th class="px-4 py-2">Name</th>
+        <th class="px-4 py-2">Score</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each sourceData as data}
+        <tr class="{data.ranking % 2  === 0 ? 'bg-surface-700' : 'bg-surface-900'}">
+            <td class="px-4 py-4 text-center">{data.ranking}</td>
+            <td class="px-4 py-4 text-center">{data.name}</td>
+            <td class="px-4 py-4 text-center">{data.score}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 </div>
 
-<div id="self">
-    <Table source={youTable}/>
-</div>
-
-<style>
-    #self {
-        background-color: "bg-surface-800";
-        width: 100%;
-        position: fixed;
-        bottom: 69px;
-        right: -5px;
-        z-index: 2;
-    }
-</style>
