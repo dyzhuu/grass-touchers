9a45d833fb35f4c708c1e1de81b3d9b5a0e9ca45
CMCheckmate <mingraygoy@gmail.com>
2023-07-08 15:47:59 +1200

    kmk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,67 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+<div class="wrapper">
+	<div class="map">
+		<div class="locationContent">
+			<button id="locationButton" on:click={getUserCoordinates}>
+				<span>Get Location</span>
+			</button>
+			<p id="locationText">Latitude:? Longitude:?</p>
+		</div>
 	</div>
 </div>
+
+<style>
+	.wrapper {
+		height: 100%;
+
+		display: grid;
+	}
+	.map {
+		margin: 5%;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		border: 5px solid black;
+		text-align: center;
+	}
+	#locationButton {
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+		background-color: lightgray;
+		color: black;
+	}
+</style>
+
+<script>
+	// Check if user's coordinates is in grid
+	// Get user's coordinates
+	// If in, turn green, if not turn red
+	// When green, allow user to take pictures, if red, don't allow user to take pictures
+	// Silo Park: 5175ed36d028d86540595ceb45eb826b42c0f00102f901a8bdef220000000092030953696c6f205061726b
+	// Waiatarau / Victoria Park: 5123f0938e21d86540594e3cbcbc676c42c0f00102f90170bb790000000000920319576169617461726175202f20566963746f726961205061726b
+	// Albert Park: 51e2534a0691d86540593bf20dd9d56c42c0f00101f901620c93000000000092030b416c62657274205061726b
+	// Pukekawa / Auckland Domain: 513d239287cfd8654059782dfb49016e42c0f00102f9015f025c000000000092031a50756b656b617761202f204175636b6c616e6420446f6d61696e
+	// Testing - OGGB: 519065e8e5add8654059bd691fe0346d42c0f00102f901bb986c010000000092031c4f77656e20472e20476c656e6e204275696c64696e67202832363029
+
+	function getUserCoordinates() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+				const locationText = document.getElementById("locationText");
+
+				// Display coordinates
+				// @ts-ignore
+				locationText.textContent = `Latitude: ${latitude} Longitude: ${longitude}`;
+			},
+			(error) => {
+				console.error("Error getting user coordinates:", error.message);
+			}
+			);
+		} else {
+			console.error("Geolocation is not supported by this browser.");
+		}
+	}
+</script>
