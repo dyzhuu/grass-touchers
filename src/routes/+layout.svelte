1196850b7f547e912ab6fc0a22afb925c1a6d261
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-07-08 17:04:52 +1200

    fixed rebase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,9 @@
-<AppShell>
-	<svelte:fragment slot="header">
-	<AppBar gridColumns="grid-cols-3" slotTrail="place-content-end">
-		<svelte:fragment slot="lead"><h1 id="titleLogo">Grass Touchers</h1></svelte:fragment>
-		<svelte:fragment slot="trail"><button id="cameraButton" on:click={uploadPicture}>Camera</button></svelte:fragment>
-	</AppBar>
-	</svelte:fragment>
-
-	<slot />
-
-	<svelte:fragment slot="footer">
-		<div class="footer">
-			<button id="mapButton" class="selected" type="button" on:click={function() {switchPage("map")}}>
-				<span>(icon)</span>
-				<span>Map</span>
-			</button>
-			<button id="leaderboardButton" type="button" on:click={function() {switchPage("leaderboard")}}>
-				<span>(icon)</span>
-				<span>Leaderboard</span>
-			</button>
-		</div>
-	</svelte:fragment>
-</AppShell>
-
-<style>
-    .footer {
-        display: grid;
-        grid-template-columns: 50% 50%;
-    }
-    .footer button {
-        padding: 1rem;
-        background-color: #8b8a8a;
-		color: white;
-		font-weight: bold;
-		font-family: "Verdana";
-		font-size: 1rem;
-    }
-    .footer button.selected {
-        background-color: #538989;
-    }
-    .footer button:hover {
-        background-color: #416a6a;
-    }
-
-	#titleLogo {
-		font-family: "Montserrat";
-		font-size: 2rem;
-	}
-
-	#cameraButton {
-		padding: 0.5rem;
-		border-radius: 0.5rem;
-		background-color: lightgray;
-		color: black;
-	}
-</style>
-
 <script lang='ts'>
 	// The ordering of these imports is critical to your app working properly
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { AppShell, AppBar, Modal, Toast } from '@skeletonlabs/skeleton';
 
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
@@ -93,7 +35,7 @@
 		<div class="h-20 flex ">
 			<a href="/play">
 				<div class="flex justify-center h-full w-[50dvw] p-1">
-					<button type="button" class="btn w-full rounded-md text-black {map ? 'bg-primary-800' : 'bg-primary-500'}" >Map</button>
+					<button type="button" class="btn w-full rounded-md text-black {map ? 'bg-primary-800': 'bg-primary-500'}" >Map</button>
 				</div>
 			</a>
 
