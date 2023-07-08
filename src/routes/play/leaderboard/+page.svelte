e450bf95c08a660bfb172722fc54a69ed0bdea58
f13322 <franck13322@gmail.com>
2023-07-08 13:53:56 +1200

    testing longer leaderboard

--- src/routes/play/leaderboard/+page.svelte
+++ src/routes/play/leaderboard/+page.svelte
@@ -2,21 +2,31 @@
     import type { TableSource } from '@skeletonlabs/skeleton';
     import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
 
-
     const sourceData = [
         { position: 1, name: 'Hydrogen', score: '100' },
         { position: 2, name: 'Helium', score: '90' },
         { position: 3, name: 'Lithium', score: '80' },
         { position: 4, name: 'Beryllium', score: '70' },
         { position: 5, name: 'Boron', score: '60' },
+        { position: 6, name: 'Hydrogen', score: '100' },
+        { position: 7, name: 'Helium', score: '90' },
+        { position: 8, name: 'Lithium', score: '80' },
+        { position: 9, name: 'Beryllium', score: '70' },
+        { position: 10, name: 'Boron', score: '60' },
+        { position: 11, name: 'Hydrogen', score: '100' },
+        { position: 12, name: 'Helium', score: '90' },
+        { position: 13, name: 'Lithium', score: '80' },
+        { position: 14, name: 'Beryllium', score: '70' },
+        { position: 15, name: 'Boron', score: '60' },
     ];
 
     const tableSimple: TableSource = {
         // A list of heading labels.
-        head: ['Name', 'Score'],
+        head: ['Rank', 'Name', 'Score'],
         // The data visibly shown in your table body UI.
-        body: tableMapperValues(sourceData, ['name', 'score']),
+        body: tableMapperValues(sourceData, ['position', 'name', 'score']),
     }     
 </script>
 
 <Table source={tableSimple}/>
+<!-- 56pix -->
