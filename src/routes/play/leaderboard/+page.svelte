4c87370cf0f352eb4d9514153c017dc50cc68f82
f13322 <franck13322@gmail.com>
2023-07-08 15:53:40 +1200

    Added table for self score

--- src/routes/play/leaderboard/+page.svelte
+++ src/routes/play/leaderboard/+page.svelte
@@ -2,6 +2,10 @@
     import type { TableSource } from '@skeletonlabs/skeleton';
     import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
 
+    const you = [
+        { position: 100, name: 'hee', score: '1' },
+    ]
+
     const sourceData = [
         { position: 1, name: 'Hydrogen', score: '100' },
         { position: 2, name: 'Helium', score: '90' },
@@ -18,15 +22,43 @@
         { position: 13, name: 'Lithium', score: '80' },
         { position: 14, name: 'Beryllium', score: '70' },
         { position: 15, name: 'Boron', score: '60' },
+        { position: 15, name: 'Boron', score: '60' },
+        { position: 15, name: 'Boron', score: '60' },
+        { position: 15, name: 'Boron', score: '60' },
+        { position: 16, name: 'Boron', score: '60' },
+        { position: 17, name: 'Boron', score: '60' },
     ];
 
-    const tableSimple: TableSource = {
+    const leaderboard: TableSource = {
         // A list of heading labels.
         head: ['Rank', 'Name', 'Score'],
         // The data visibly shown in your table body UI.
         body: tableMapperValues(sourceData, ['position', 'name', 'score']),
-    }     
+    }
+
+    const youTable: TableSource = {
+        // A list of heading labels.
+        head: [],
+        // The data visibly shown in your table body UI.
+        body: tableMapperValues(you, ['position', 'name', 'score']),
+    }
 </script>
 
-<Table source={tableSimple}/>
-<!-- 56pix -->
+<div id="global">
+    <Table source={leaderboard}/>
+</div>
+
+<div id="self">
+    <Table source={youTable}/>
+</div>
+
+<style>
+    #self {
+        background-color: "bg-surface-800";
+        width: 100%;
+        position: fixed;
+        bottom: 69px;
+        right: -5px;
+        z-index: 2;
+    }
+</style>
