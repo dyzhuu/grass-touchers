b61af1dd434ce7c6c7541099658c273a7a677518
f13322 <franck13322@gmail.com>
2023-07-08 13:32:39 +1200

    Added leaderboard page with dummy data

--- src/routes/play/leaderboard/+page.svelte
+++ src/routes/play/leaderboard/+page.svelte
@@ -1,1 +1,22 @@
-<p>asfjh</p>
+<script lang="ts">
+    import type { TableSource } from '@skeletonlabs/skeleton';
+    import { Table, tableMapperValues } from '@skeletonlabs/skeleton'
+
+
+    const sourceData = [
+        { position: 1, name: 'Hydrogen', score: '100' },
+        { position: 2, name: 'Helium', score: '90' },
+        { position: 3, name: 'Lithium', score: '80' },
+        { position: 4, name: 'Beryllium', score: '70' },
+        { position: 5, name: 'Boron', score: '60' },
+    ];
+
+    const tableSimple: TableSource = {
+        // A list of heading labels.
+        head: ['Name', 'Score'],
+        // The data visibly shown in your table body UI.
+        body: tableMapperValues(sourceData, ['name', 'score']),
+    }     
+</script>
+
+<Table source={tableSimple}/>
