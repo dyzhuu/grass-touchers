24a53d4bb807180d1c3a4029d45df6cf08b3d489
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-07-08 17:31:12 +1200

    fixed buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import "../app.postcss" 
 	import { Toast, Modal } from '@skeletonlabs/skeleton';
-</script>
 
 <Modal zIndex="98" />
-<Toast zIndex="99" />
+	<Toast zIndex="99" />
 
-<slot />
+</script>
