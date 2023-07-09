ffed489423a1a913caa19da9cce468239c6f17ec
Nathan  Hare <me@laspruca.nz>
2023-07-09 13:33:13 +1200

    Get visited locations

--- src/routes/api/locations/+server.ts
+++ src/routes/api/locations/+server.ts
@@ -1,25 +1,12 @@
-import { getUser, getVisitedLocations } from '$lib/server/db';
+import { getLocations, getUser, getVisitedLocations } from '$lib/server/db';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ locals }) => {
-	let user;
-
-	try {
-		user = await getUser(locals.user!.email!);
-	} catch (ex) {
-		console.error(ex);
-		throw error(500, 'internal error');
-	}
-
-	if (!user) {
-		throw error(404, 'User does not have a profile');
-	}
-
+export const GET: RequestHandler = async ({}) => {
 	let locations;
 
 	try {
-		locations = await getUser(locals.user!.email!);
+		locations = await getLocations();
 	} catch (ex) {
 		console.error(ex);
 		throw error(500, 'internal error');
---
+++ src/routes/api/secure/visited/+server.ts
@@ -0,0 +1,29 @@
+import { getUser, getVisitedLocations } from '$lib/server/db';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ locals }) => {
+	let user;
+
+	try {
+		user = await getUser(locals.user!.email!);
+	} catch (ex) {
+		console.error(ex);
+		throw error(500, 'internal error');
+	}
+
+	if (!user) {
+		throw error(404, 'User does not have a profile');
+	}
+
+	let locations;
+
+	try {
+		locations = await getVisitedLocations(user.id!);
+	} catch (ex) {
+		console.error(ex);
+		throw error(500, 'internal error');
+	}
+
+	return json(locations);
+};
